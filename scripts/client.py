2cc4dde7a0d25fc90462721a411ed75c18f8823a
¨Murtaza <mdalal@berkeley.edu>
2017-08-27 16:39:10 -0700

    got gravity torques working on the sawyer

--- scripts/client.py
+++ scripts/client.py
@@ -9,11 +9,12 @@ def gravity_torques_client():
     try:
         gravity_torques = rospy.ServiceProxy('gravity_torques', GravityTorques)
         resp1 = gravity_torques()
-        return resp1.torques
+        return resp1.gravity_torques, resp1.hysteresis_torques, resp1.crosstalk_torques, resp1.actual_torques, resp1.subtracted_torques
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
 if __name__ == "__main__":
     # print(gravity_torques_client('right'))
-    for _ in range(1000):
-        print(gravity_torques_client())
+    for _ in range(1):
+        print(gravity_torques_client()[4
+              ])
--- scripts/server.py
+++ scripts/server.py
@@ -6,13 +6,19 @@ import numpy as np
 from intera_core_msgs.msg import SEAJointState
 
 def handle_gravity_torques(req):
-    return GravityTorquesResponse(gravity_torques, actual_effort)
+    return GravityTorquesResponse(gravity_torques, actual_torques, hysteresis_torques, crosstalk_torques, subtracted_torques)
 
 def gravity_torques_callback(data):
     global gravity_torques
-    global actual_effort
+    global actual_torques
+    global subtracted_torques
+    global hysteresis_torques
+    global crosstalk_torques
     gravity_torques = np.array(data.gravity_model_effort)
-    actual_effort = np.array(data.actual_effort)
+    actual_torques = np.array(data.actual_effort)
+    hysteresis_torques = np.array(data.hysteresis_model_effort)
+    crosstalk_torques = np.array(data.crosstalk_model_effort)
+    subtracted_torques = actual_torques - gravity_torques - crosstalk_torques - hysteresis_torques
 
 def gravity_torques_server():
     rospy.init_node('gravity_torques_server')
